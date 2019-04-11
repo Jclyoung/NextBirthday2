f5a2bbdd515d070e2577ce58e4a7983d394353b1
ccartagena879 <ccartagena879@gmail.com>
2019-04-10 19:49:43 -0500

    finished

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -6,9 +6,23 @@ namespace NextBirthday
     {
         public static void Main(string[] args)
         {
+            
+            Console.WriteLine("Enter your date of birth: ");
+
+            string dateOfBirth = Console.ReadLine();
+            DateTime newDateOfBirth = DateTime.Parse(dateOfBirth);
+            newDateOfBirth = newDateOfBirth.AddYears(1);
+            Console.WriteLine(NextBirthdayDay(newDateOfBirth));
             Console.ReadLine();
         }
 
         // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
+
+        public static string NextBirthdayDay(DateTime newDateOfBirth)
+        {
+                return newDateOfBirth.DayOfWeek.ToString();
+        }
+
+
     }
 }
