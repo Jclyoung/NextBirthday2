eeb52c40fd43426ff8dd463b1e09380f6c759e7b
Jennifer Young <jclyoung@gmail.com>
2019-04-11 21:10:39 -0500

    commit

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -15,9 +15,7 @@ namespace NextBirthday
             Console.WriteLine(NextBirthdayDay(newDateOfBirth));
             Console.ReadLine();
         }
-
-        // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
-
+         // TODO: Create a method that takes the date someone was born and returns the name of the day on their next birthday.
         public static string NextBirthdayDay(DateTime newDateOfBirth)
         {
                 return newDateOfBirth.DayOfWeek.ToString();
